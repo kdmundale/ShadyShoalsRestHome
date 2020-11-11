b09f6b16d7f017c6bd7cb37f78b3a6f52cef87e2
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-11-11 09:03:12 -0500

    added a check to the login to make sure the user account has been approved included error message and redirect if it has not been adjusted mock data

--- db/login-inc.php
+++ db/login-inc.php
@@ -35,27 +35,36 @@ if (isset($_POST['submit'])) {
       $_SESSION['sessionId']= $row['id'];
       $_SESSION['sessionRole']= $row['role'];
       $_SESSION['userName']=$row['first_name']." ".$row['last_name'];
-      $role = $_SESSION['sessionRole'];
-        if ($role ==1) {
-        header("Location: ../views/aHome.php?success=login");
-        exit();
-      } elseif ($role == 2){
-        header("Location: ../views/sHome.php?success=login");
-        exit();
-      } elseif ($role == 3) {
-        header("Location: ../views/dHome.php?success=login");
-        exit();
-      } elseif ($role == 4) {
-        header("Location: ../views/cHome.php?success=login");
-        exit();
-      } elseif ($role ==5 || $role ==6) {
-        header("Location: ../views/oHome.php?success=login");
-        exit();
-      } else {
-        echo "There was problem logging in.";
+      $_SESSION['approved']= $row['approved'];
+      $approved = $_SESSION['approved'];
+      if ($approved == 0) {
+        echo "Thanks for stopping by, ".$_SESSION['userName'].", but your account has not yet been approved. Please check back later.";
         echo "<br/>";
         echo "<a href='../index.php'>Go back</a>";
         exit();
+      } elseif ($approved == 1) {
+        $role = $_SESSION['sessionRole'];
+          if ($role ==1) {
+          header("Location: ../views/aHome.php?success=login");
+          exit();
+        } elseif ($role == 2){
+          header("Location: ../views/sHome.php?success=login");
+          exit();
+        } elseif ($role == 3) {
+          header("Location: ../views/dHome.php?success=login");
+          exit();
+        } elseif ($role == 4) {
+          header("Location: ../views/cHome.php?success=login");
+          exit();
+        } elseif ($role ==5 || $role ==6) {
+          header("Location: ../views/oHome.php?success=login");
+          exit();
+        } else {
+          echo "There was problem logging in.";
+          echo "<br/>";
+          echo "<a href='../index.php'>Go back</a>";
+          exit();
+        }
       }
     } else {
       echo "Either the email or password was incorrect.";
