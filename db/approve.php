5da39385ccb8af8738435dcc6392511d8e74e292
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-11-12 19:48:54 -0500

    added styling to the approval page, changed the toggle to checkmarks with a single submit button added a hidden input to carry the userId for each row

--- db/approve.php
+++ db/approve.php
@@ -2,8 +2,16 @@
 if (isset($_POST['submit'])) {
 
   require 'db.php';
+  require "../includes/head.php";
+  require "../includes/header.php";
 
   $approval = $_POST['approval'];
+
+  if ($approval==1) {
+    $pageTitle = "Already Approved";
+  } else {
+    $pageTitle = "Pending Approval";
+  }
 
   if(mysqli_connect_errno()){
     die("connection failed: "
@@ -25,16 +33,16 @@ mysqli_stmt_bind_param($stmt, "i", $approval);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 //showing property
-echo '<?php require "../includes/head.php";require "../includes/header.php";?>';
-echo '<h1>Results</h1>';
+
+echo "<h1>Users ".$pageTitle."</h1>";
 echo '<table class="data-table">
         <tr class="data-heading">';  //initialize table tag
 while ($property = mysqli_fetch_field($result)) {
     echo "<td style='font-size:18; font-weight:bold'>" . $property->name . '</td>';  //get field name for header
     array_push($all_property, $property->name);  //save those to array
 }
-echo "<td style='font-size:18; font-weight:bold'> toggle approval </td>";
-echo "<td style='font-size:18; font-weight:bold'> change </td>";
+echo "<td style='font-size:18; font-weight:bold'></td>";
+// echo "<td style='font-size:18; font-weight:bold'> change </td>";
 echo '</tr>'; //end tr tag
 //showing all data
 while ($row = mysqli_fetch_array($result)) {
@@ -42,12 +50,14 @@ while ($row = mysqli_fetch_array($result)) {
     foreach ($all_property as $item) {
         echo "<td style='border:1px solid black; font-size:18; padding:5px'>" . $row[$item] . '</td>'; //get items using property value
     }
-    echo '<td> <select name="toggle"> <option value=0>not approved</option><option value=1>approved</option></select></td>';
-    echo '<td><button type="submit" name="submit">change</button></td>';
-    echo '</tr></form>';
+    echo "<td> <div><input type='checkbox' id='yes' name='yes' value='yes' checked><label for='yes'>Approved</label></div><div><input type='checkbox' id='no' name='no' value='no'><label for='no'>Not Approved</label></div></td>";
+    echo "<td><input id='userId' name='id' type='hidden' value=".$row['id']."></td>";
+    echo "</tr>";
 }
 echo "</table>";
+echo "<button type='submit' name='submit'>change</button></form>";
 }
 }
+require "../includes/footer.php";
 
 ?>
